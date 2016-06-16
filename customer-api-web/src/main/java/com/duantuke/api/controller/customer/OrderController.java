ca087ccf53f01d5c703bfdfddc6579f65524aee5
须俊杰 <xujunjie@vip.163.com>
2016-06-16 15:05:04 +0800

    订单详情查询

--- customer-api-web/src/main/java/com/duantuke/api/controller/customer/OrderController.java
+++ customer-api-web/src/main/java/com/duantuke/api/controller/customer/OrderController.java
@@ -12,13 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.alibaba.fastjson.JSON;
 import com.duantuke.api.domain.common.OpenResponse;
 import com.duantuke.api.util.DateUtil;
 import com.duantuke.api.util.TokenUtil;
 import com.duantuke.order.common.enums.OrderErrorEnum;
+import com.duantuke.order.model.Base;
 import com.duantuke.order.model.CreateOrderRequest;
 import com.duantuke.order.model.CreateOrderResponse;
 import com.duantuke.order.model.Header;
@@ -27,7 +30,6 @@ import com.duantuke.order.model.QueryOrderRequest;
 import com.duantuke.order.model.Request;
 import com.duantuke.order.model.Response;
 import com.duantuke.order.service.OrderService;
-import com.google.gson.Gson;
 
 @Controller
 @RequestMapping(value = "/customer/order")
@@ -44,7 +46,7 @@ public class OrderController {
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = "/create")
+	@RequestMapping(method = RequestMethod.POST, value = "/create")
 	public ResponseEntity<OpenResponse<CreateOrderResponse>> create(HttpServletRequest request,
 			HttpServletResponse response) {
 		logger.info("接收到创建订单请求,request = {}", JSON.toJSONString(request));
@@ -76,17 +78,17 @@ public class OrderController {
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = "/queryOrders")
+	@RequestMapping(method = RequestMethod.POST, value = "/list")
 	public ResponseEntity<OpenResponse<List<Order>>> queryOrders(HttpServletRequest request,
-			HttpServletResponse response){
-		logger.info("接收到查询订单请求,入参:{}",JSON.toJSONString(request.getParameterMap()));
+			HttpServletResponse response) {
+		logger.info("接收到查询订单列表请求,入参:{}", JSON.toJSONString(request.getParameterMap()));
 		OpenResponse<List<Order>> openResponse = new OpenResponse<List<Order>>();
 		try {
 			String pageNo = request.getParameter("pageNo");
 			String pageSize = request.getParameter("pageSize");
 			String beginTime = request.getParameter("beginTime");
 			String endTime = request.getParameter("endTime");
-			
+
 			Request<QueryOrderRequest> req = new Request<QueryOrderRequest>();
 			Header header = new Header();
 			header.setTimeStamp(new Date());
@@ -110,14 +112,55 @@ public class OrderController {
 			openResponse.setData(res.getData());
 			openResponse.setErrorCode(res.getErrorCode());
 			openResponse.setErrorMessage(res.getErrorMessage());
-			logger.info("查询订单成功,返回结果:{}",JSON.toJSONString(openResponse));
+			logger.info("查询订单列表成功,返回结果:{}", JSON.toJSONString(openResponse));
 		} catch (Exception e) {
-			logger.error("查询订单异常", e);
+			logger.error("查询订单列表异常", e);
 			openResponse.setResult(Boolean.toString(false));
 			openResponse.setErrorCode(OrderErrorEnum.customError.getErrorCode());
 			openResponse.setErrorMessage(OrderErrorEnum.customError.getErrorMsg());
 		}
-		
+
 		return new ResponseEntity<OpenResponse<List<Order>>>(openResponse, HttpStatus.OK);
+	}
+
+	/**
+	 * 根据订单号查询订单详情
+	 * 
+	 * @param request
+	 * @param response
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "{id}")
+	public ResponseEntity<OpenResponse<Order>> queryOrder(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("id") Long id) {
+		logger.info("接收到查询订单请求,入参:{}", JSON.toJSONString(request.getParameterMap()));
+		OpenResponse<Order> openResponse = new OpenResponse<Order>();
+		try {
+
+			Request<Base> req = new Request<Base>();
+			Header header = new Header();
+			header.setTimeStamp(new Date());
+			header.setToken("token");
+			req.setHeader(header);
+
+			Base base = new Base();
+			base.setOrderId(id);
+			req.setData(base);
+
+			Response<Order> res = orderService.queryOrderByOrderId(req);
+			openResponse.setResult(Boolean.toString(res.isSuccess()));
+			openResponse.setData(res.getData());
+			openResponse.setErrorCode(res.getErrorCode());
+			openResponse.setErrorMessage(res.getErrorMessage());
+			logger.info("查询订单详情成功,返回结果:{}", JSON.toJSONString(openResponse));
+		} catch (Exception e) {
+			logger.error("查询订单详情异常", e);
+			openResponse.setResult(Boolean.toString(false));
+			openResponse.setErrorCode(OrderErrorEnum.customError.getErrorCode());
+			openResponse.setErrorMessage(OrderErrorEnum.customError.getErrorMsg());
+		}
+
+		return new ResponseEntity<OpenResponse<Order>>(openResponse, HttpStatus.OK);
 	}
 }
