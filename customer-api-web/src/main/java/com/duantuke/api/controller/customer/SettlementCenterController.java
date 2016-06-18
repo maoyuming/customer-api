f0a691011c969b4d28c4a085bcb8820fc9659e2a
lindi <lindi0619@126.com>
2016-06-18 15:02:56 +0800

    结算类文件提交

---
+++ customer-api-web/src/main/java/com/duantuke/api/controller/customer/SettlementCenterController.java
@@ -0,0 +1,10 @@
+package com.duantuke.api.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/customer/sc")
+public class SettlementCenterController {
+
+}
