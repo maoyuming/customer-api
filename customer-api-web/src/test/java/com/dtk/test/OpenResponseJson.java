4644400a55861456462d7aaa494167c94263af09
jjh <68562450@qq.com>
2016-06-23 16:51:19 +0800

    更新生成的json

--- customer-api-web/src/test/java/com/dtk/test/OpenResponseJson.java
+++ customer-api-web/src/test/java/com/dtk/test/OpenResponseJson.java
@@ -1,13 +1,13 @@
 package com.dtk.test;
 
 import com.alibaba.fastjson.JSON;
+import com.duantuke.api.domain.common.OpenResponse;
 import com.duantuke.basic.enums.SkuTypeEnum;
+import com.duantuke.order.common.enums.OrderErrorEnum;
 import com.duantuke.order.common.enums.OrderStatusEnum;
 import com.duantuke.order.common.enums.PayStatusEnum;
 import com.duantuke.order.common.enums.PayTypeEnum;
-import com.duantuke.order.model.Order;
-import com.duantuke.order.model.OrderDetail;
-import com.duantuke.order.model.OrderDetailPrice;
+import com.duantuke.order.model.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -15,12 +15,16 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Created by jjh on 16/6/17.
+ * Created by jjh on 16/6/22.
  */
-public class OrderJson {
+public class OpenResponseJson {
 
     public static void main(String[] args) {
+        OpenResponse<Order> openResponse = new OpenResponse<Order>();
+
+        List<Order> orderList = new ArrayList<Order>();
         Order order = new Order();
+        order.setId(11111L);
         order.setType(0);
         order.setStatus(OrderStatusEnum.confirmed.getId());
         order.setPayType(PayTypeEnum.prepay.getId());
@@ -38,30 +42,40 @@ public class OrderJson {
 
         List<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
         OrderDetail orderDetail1 = new OrderDetail();
+        orderDetail1.setId(2222L);
         orderDetail1.setSkuId(789L);
         orderDetail1.setSkuName("商品");
         orderDetail1.setSkuType(SkuTypeEnum.roomtype.getCode());
         orderDetail1.setNum(1);
-        orderDetail1.setTotalPrice(new BigDecimal(100));
-//        orderDetail1.setPrice(new BigDecimal(100));
+        orderDetail1.setTotalPrice(new BigDecimal(33333));
 
         List<OrderDetailPrice> priceDetails = new ArrayList<OrderDetailPrice>();
         OrderDetailPrice orderDetailPrice = new OrderDetailPrice();
         orderDetailPrice.setActionTime(new Date());
+        orderDetailPrice.setCreateBy("11111");
+        orderDetailPrice.setCreateTime(new Date());
+        orderDetailPrice.setOrderDetailId(2222L);
+        orderDetailPrice.setOrderId(11111L);
         orderDetailPrice.setPrice(new BigDecimal(123));
         orderDetailPrice.setSkuId(789L);
         orderDetailPrice.setSkuName("商品");
+        orderDetailPrice.setUpdateBy("11111");
+        orderDetailPrice.setUpdateTime(new Date());
         priceDetails.add(orderDetailPrice);
 
-        orderDetail1.setPriceDetails(priceDetails);
 
+        orderDetail1.setPriceDetails(priceDetails);
         orderDetailList.add(orderDetail1);
 
         order.setOrderDetails(orderDetailList);
+        orderList.add(order);
 
-        System.out.println(JSON.toJSONString(order));
+        openResponse.setResult("true");
+        openResponse.setData(order);
+        openResponse.setErrorCode(OrderErrorEnum.paramsError.getErrorCode());
+        openResponse.setErrorMessage(OrderErrorEnum.paramsError.getErrorMsg());
 
-        // {"beginTime":1466241251395,"contact":"jjh","contactPhone":"123456","customerId":123,"endTime":1466241251395,"memo":"beizhu","orderDetails":[{"num":1,"price":100,"skuId":789,"skuName":"商品","skuType":1}],"payStatus":10,"payType":100,"promotionId":321,"status":50,"supplierId":456,"supplierName":"sky","totalPrice":100,"type":0}
+        System.out.println(JSON.toJSONString(openResponse));
     }
 
 }
